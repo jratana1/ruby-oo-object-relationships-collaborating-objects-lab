5e5ba79d2b87679042a5eed92fdfba55bcb76ccc
John Ratana <jratana1@gmail.com>
2020-10-26 22:13:09 -0400

    commit tree 80e2f4962d3ddb06f969df1c9f273f1506e15e07

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+class MP3Importer
+    attr_accessor :path
+
+    def initialize (path)
+        @path = path
+    end
+
+    def files
+        files = Dir.glob("**/*.mp3")
+        files.map {|file| File.basename(file)}
+    end
+
+    def import
+        self.files.each {|file| Song.new_by_filename(file)}
+    end
+end
